85d419d69aab21f8996549fae0a65cce94bfc735
Joy <mbuvimumbuambua99@gmail.com>
2024-10-08 12:36:40 +0300

    changed favorites/views.py

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -1,43 +1,44 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import FavouritesSerializer  
 from .models import Favourites
 
+@api_view(['POST'])
 def add_to_favourites(request):      
     user = request.user.is_authenticated
     if not user:
-        return JsonResponse({'error': "not authenticated"}, status=401)
+        return Response({'error': "not authenticated"}, status=401)
     
-    if request.method == 'POST':
-        fave = FavouritesSerializer(data=request.POST)  
-        if fave.is_valid():
+    fave = FavouritesSerializer(data=request.data)  
+    if fave.is_valid():
             fave.save()  
-            return JsonResponse({'message': 'Favourite added'}, status=201)  
-        else:
-            return JsonResponse({'errors': fave.errors}, status=400)  
+            return Response({'message': 'Favourite added'}, status=201)  
+    else:
+            return Response({'errors': fave.errors}, status=400)  
 
+@api_view(['GET'])
 def get_favourites(request):
     user=request.user.is_authenticated
     if not user:
-        return JsonResponse({'error':'not authenticated'}, status=401)
+        return Response({'error':'not authenticated'}, status=401)
 
-    if request.method == 'GET':
-        favs= Favourites.objects.filter(user=request.user)
-        serializer=FavouritesSerializer(favs,many=True)
-        return JsonResponse(serializer.data,status=200,safe=False)
+    favs= Favourites.objects.filter(user=request.user)
+    serializer=FavouritesSerializer(favs,many=True)
+    return Response(serializer.data,status=200,safe=False)
 
-
+@api_view(['DELETE'])
 def delete_favourites(request,recipe_name):
     user=request.user.is_authenticated
     if not user:
-        return JsonResponse({'error':'not authenticated'}, status=401)
+        return Response({'error':'not authenticated'}, status=401)
 
     if request.method == 'DELETE':
      try:
         faves=Favourites.objects.get(recipe_name=recipe_name,user=request.user)
         faves.delete()
-        return JsonResponse({'message': 'Favourite deleted'}, status=204)  
+        return Response({'message': 'Favourite deleted'}, status=204)  
      except Favourites.DoesNotExist:
-        return JsonResponse({'error':'no favourites found'}, status=404)
+        return Response({'error':'no favourites found'}, status=404)
     
-    return JsonResponse({'error': 'Method not allowed'}, status=405) 
+    return Response({'error': 'Method not allowed'}, status=405) 
 
